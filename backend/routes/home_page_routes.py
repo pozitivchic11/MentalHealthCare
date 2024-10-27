100d7034cd33326a370168ab8eb9643e9fdc60e8
Oleksandr Rybka <olecksandr.rybka@ukr.net>
2024-10-27 20:23:14 +0200

    Implemented login process in login form and its appereance

--- backend/routes/home_page_routes.py
+++ backend/routes/home_page_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
 from backend.src.signup import register_user
+from backend.src.login import login_user
 
 home_page_routes = Blueprint('api', __name__)
 
@@ -9,9 +10,13 @@ def home_page():
     return render_template('index.html')
 
 
-@home_page_routes.route('/login', methods=['GET', 'POST'])
+@home_page_routes.route('/login', methods=['POST'])
+
 def login():
-    return render_template('login.html')
+    data = request.get_json()
+    http_status = login_user(data.get('email'), data.get('password'))
+
+    return jsonify({'message': 'User logged in successfully!'}), http_status
 
 @home_page_routes.route('/signup', methods=['POST'])
 
@@ -20,3 +25,8 @@ def signup():
     http_status = register_user(data.get('name'), data.get('surname'), data.get('email'), data.get('password'))
 
     return jsonify({'message': 'User created successfully!'}), http_status
+
+@home_page_routes.route('/test')
+
+def test_page():
+    return render_template('test.html')
---
+++ backend/src/login.py
@@ -0,0 +1,10 @@
+from backend.db.db_manager import execute_query
+
+def login_user(email, password):
+    existing_user_query = f"SELECT * FROM users WHERE email = '{email}' AND password = '{password}'"
+    existing_user = execute_query(existing_user_query, query_type="select")
+
+    if existing_user:
+        return 200
+
+    return 401
